6753e1a8d7970b72bd05b5fe88e1e08956e029f0
Kim Jisena <pjohmaths@gmail.com>
2022-08-28 17:04:18 +0300

    implement quick sort

---
+++ quick-sort/quick-sort.js
@@ -0,0 +1,23 @@
+function quickSort(array) {
+  // Only change code below this line
+  if (array.length === 0 || array.length === 1) {
+    return array;
+  }
+
+  let pivotIdx = 0;
+  let left = [], right = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i === pivotIdx) continue;
+
+    if (array[i] > array[pivotIdx]) {
+      right.push(array[i]);
+    } else {
+      left.push(array[i]);
+    }
+  }
+  left = quickSort(left);
+  right = quickSort(right);
+  array = left.concat([array[pivotIdx]]).concat(right);
+  return array;
+  // Only change code above this line
+}
